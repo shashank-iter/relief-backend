8376827a9a3e55ba8e825651566d114cf573ad90
shashank-iter <142831585+shashank-resellpur@users.noreply.github.com>
2025-04-26 03:04:57 +0530

    feat: add routes to patient controllers

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import { upload } from "../middlewares/multer.middleware.js";
 import {
   registerGeneralUser,
   loginGeneralUser,
+  getUserData,
+  updatePatientProfile,
+  getPatientProfile, 
+  updateEmergencyContact, 
+  updateMedicalHistory,
+  deleteEmergencyContact,
+  deleteMedicalHistoryItem
 } from "../controllers/user.controller.js";
 
 const admin = ["admin"];
@@ -14,5 +21,19 @@ const router = Router();
 
 router.route("/register").post(registerGeneralUser);
 router.route("/login").post(loginGeneralUser);
+// router.route("/profile").get(verifyJWT(user), getUserData);
+
+// Profile routes
+router.route("/profile").get(verifyJWT(user), getPatientProfile);
+router.route("/update-profile").put(verifyJWT(user), updatePatientProfile);
+
+
+// Emergency contact routes
+router.route("/emergency-contacts").post(verifyJWT(user), updateEmergencyContact);
+router.route("/emergency-contacts/:contactId").delete(verifyJWT(user), deleteEmergencyContact);
+
+// Medical history routes
+router.route("/medical-history/:type").post(verifyJWT(user), updateMedicalHistory);
+router.route("/medical-history/:type/:itemId").delete(verifyJWT(user), deleteMedicalHistoryItem);
 
 export default router;
