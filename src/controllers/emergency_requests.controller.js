eaf5a9a1bb26448c5608ceb2fc172e04f2557b1c
shashank-iter <pandeyshashank812@gmail.com>
2025-06-08 05:58:06 +0530

    feat: improve get hospital response for patient

--- src/controllers/emergency_requests.controller.js
+++ src/controllers/emergency_requests.controller.js
@@ -146,11 +146,11 @@ const getHospitalResponsesForPatient = asyncHandler(async (req, res) => {
 
   return res.status(200).json(
     new ApiResponse(200, "Accepted hospitals fetched successfully", {
-      requestId: emergencyRequest._id,
-      acceptedBy: emergencyRequest.acceptedBy,
+      request: emergencyRequest,
     })
   );
 });
+
 
 
 const getEmergencyRequestsByStatusForPatient = asyncHandler(async (req, res) => {
